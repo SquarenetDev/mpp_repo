f27e0505be2ca3bb83ec1d0dfe93b094ac410d77
Your Name <you@example.com>
2019-09-26 11:48:49 +0900

    modi

--- popup_data/backup.sh
+++ popup_data/backup.sh
@@ -17,7 +17,7 @@ echo '---------------------------------- file backup start'
 sudo cp /edx/app/edxapp/edx-platform/lms/envs/common.py /edx/app/edxapp/edx-platform/lms/envs/org_common.py
 sudo cp /edx/app/edxapp/edx-platform/lms/urls.py /edx/app/edxapp/edx-platform/lms/org_urls.py
 sudo cp -r /edx/app/edxapp/edx-platform/lms/djangoapps /edx/app/edxapp/edx-platform/lms/org_djangoapps
-sudo cp -r /edx/app/edxapp/themes/$theme_name/edx/app/edxapp/themes/org_$theme_name
+sudo cp -r /edx/app/edxapp/themes/$theme_name /edx/app/edxapp/themes/org_$theme_name
 
 echo '---------------------------------- file backup done'
 
